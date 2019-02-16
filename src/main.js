6bd8f3fa0db59e8947d6e0d38d4f5d747aed69be
小霸王 <841868101@qq.com>
2019-02-16 15:42:40 +0800

    首页功能完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/global.css'
+import axios from 'axios'
+// 引入字体图标
+import './assets/fonts/iconfont.css'
+// 引入element-ui组件库模块
+import ElementUI from 'element-ui'
+import Promises from 'fs'
+// 把element-ui注册给vue
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+
+// axios的请求拦截器（在其中配置token）
+axios.interceptors.request.use(
+  function(config) {
+    // config:代表axios的子级配置对象
+    var token = window.sessionStorage.getItem('token')
+    config.headers.Authorization = token
+    return config
+  },
+  function(error) {
+    return Promises.reject(error)
+  }
+)
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../components/Login'
+import home from '../components/home'
+import Welcome from '../components/Welcome'
+import User from '../components/User'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: User }
+      ]
     }
   ]
 })
+
+// 给路由设置“导航守卫”
+// 在守卫中对token进行监听，有token就让执行，否则跳回登录页面
+router.beforeEach((to, from, next) => {
+  // 请求login就直接通过
+  if (to.path === '/login') {
+    return next()
+  }
+  // 请求非login就判断token
+  var token = window.sessionStorage.getItem('token')
+  if (!token) {
+    return next('/login')
+  }
+  next() // 请继续你的旅行
+})
+export default router
